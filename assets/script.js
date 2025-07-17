d88cedac3b4f0313010a6c13553b442362cdea35
paullorenzopsima@gmail.com <jericho@gestafi.com>
2025-07-17 17:49:25 +0800

    Add Clear history button

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
+
+
 const equal = document.querySelector('#equal')
 const integer = document.querySelector('#integer')
 const AC = document.querySelector('#AC')
 const backspace = document.querySelector('#backspace')
 const percentage = document.getElementById('percentage')
 const digits = document.querySelectorAll('.primaryDisplay')
-const operators = ['+', '-','*','/','.','%']
+const operators = ['+', '-','*','/','.','%',]
 const history = document.getElementById('historybox')
 const historycontainer = document.getElementById('historylogcontainer')
+const historylist = []
+
 let calc = "";
 
 digits.forEach(btns => {
@@ -23,15 +27,10 @@ function btnsprimary (buttons) {
     display.textContent = calc
 }
 
-equal.addEventListener('click', function (){
-  try {
-    calc = parseFloat(eval(calc)).toString()
-    display.textContent = calc
-  } catch {
-    display.textContent = "Error"
-    calc = ""
-  }
-})
+integer.addEventListener('click', () => {
+  calc = calc.startsWith('-') ? calc.slice(1) : '-' + calc;
+  display.textContent = calc;
+  })
 
 backspace.addEventListener('click', function () {
     calc = calc.toString().slice(0, -1)
@@ -54,6 +53,18 @@ percentage.addEventListener('click', () => {
   }
 })
 
+equal.addEventListener('click', function (){
+  try {
+    const result = parseFloat(eval(calc)).toString()
+    historylist.push(`${calc} = ${result}`)
+    calc = result
+    display.textContent = calc
+
+  } catch {
+    display.textContent = "Error"
+    calc = ""
+  }
+})
 
 // When the history is clicked -> creates a div class -> shows the user's calculations -> clear history button -> no history
 
@@ -62,4 +73,37 @@ percentage.addEventListener('click', () => {
 
 history.addEventListener('click', () => {
   historycontainer.classList.toggle('show')
+  historycontainer.innerHTML = ''
+
+
+  const historycontent = document.createElement('div')
+  historycontent.className='historycontent'
+  historycontent.style.textAlign='end'
+  historycontainer.append(historycontent)
+  
+
+  const clearButton = document.createElement('button')
+  clearButton.id('clearbtn')
+  clearButton.textContent = 'Clear History'
+  clearButton.style.marginbottom = '10px'
+  historycontainer.append(clearButton)
+
+
+  if (historylist.length === 0) {
+    historylist.textContent = 'No History'
+  } else {
+    historylist.forEach(entry => {
+      const entryDiv = document.createElement('div')
+      entryDiv.textContent = entry
+      historycontent.append(entryDiv)
+    })
+  }
+
+  clearButton.addEventListener ('click', () => {
+    historylist.length = 0
+    historycontent.textContent = "No History"
+  })
 })
+
+
+
