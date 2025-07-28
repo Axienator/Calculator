0538b461fea7efc3c3421e4602dfcffcd9330346
paullorenzopsima@gmail.com <jericho@gestafi.com>
2025-07-28 10:16:59 +0800

    Add vercel.json file

--- assets/theme.js
+++ assets/theme.js
@@ -1,6 +1,5 @@
 const display = document.getElementById('box')
 const containermode = document.getElementById('mode')
-const contain = document.getElementById('contain')
 const moonsym = document.getElementById('moonsym')
 const colorbody = document.getElementById('contain')
 const body = document.body
@@ -15,8 +14,9 @@ containermode.addEventListener("click", function(){
  if (moonsym.classList.contains('nightmode')) {
   moonsym.textContent = "☾"
   colorbody.style.backgroundColor='antiquewhite'
+  colorbody.style.border='2px solid black'
   containermode.style.backgroundColor='black'
-  body.style.backgroundColor='black'
+  body.style.backgroundColor='rgba(46, 46, 46, 1)'
   display.style.backgroundColor='black'
   display.style.color='antiquewhite'
   historycontainer.style.backgroundColor='rgb(136, 127, 116)'
@@ -33,7 +33,8 @@ containermode.addEventListener("click", function(){
     moonsym.textContent = "☀"
     containermode.style.backgroundColor='antiquewhite'
     colorbody.style.backgroundColor='black'
-    body.style.backgroundColor='antiquewhite'
+    colorbody.style.border='2px solid rgb(238, 216, 187)'
+    body.style.backgroundColor='rgb(179, 179, 179)'
     display.style.backgroundColor='antiquewhite'
     display.style.color='black'
     historycontainer.style.backgroundColor='rgb(44, 42, 39)'
